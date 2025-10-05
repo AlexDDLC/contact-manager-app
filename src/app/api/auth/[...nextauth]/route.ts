17e945c35b95c28a0b82d2bfd90ee639476ad4a5
AlexDDLC <alexddlc139@gmail.com>
2025-10-05 01:09:13 -0400

    [UPD] Improve login logic

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { prisma } from '@/lib/prisma'
+import bcrypt from 'bcryptjs'
 
 const authOptions = {
     providers: [
@@ -11,15 +12,27 @@ const authOptions = {
                 password: { label: "Contraseña", type: "password", placeholder: "*************" }
             },
             async authorize(credentials, request) {
-                console.log(credentials)
+                if (!credentials?.email || !credentials?.password) {
+                    return null
+                }
 
-                const userFound = await prisma.user.findUnique({
+                const userExists = await prisma.user.findUnique({
                     where: { email: credentials?.email }
                 })
 
-                console.log(userFound);
+                if (!userExists) throw new Error('Error en el Usuario o Contraseña')
 
-                return null;
+                const passwordMatch = await bcrypt.compare(
+                    credentials.password,
+                    userExists.password
+                )
+
+                if (!passwordMatch) throw new Error('Error en el Usuario o Contraseña')
+
+                return {
+                    id: userExists.id.toString(),
+                    email: userExists.email
+                }
             }
         })
     ]
