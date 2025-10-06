53805bac7b3975aa0a1047ea5de7ea970184a3d7
AlexDDLC <alexddlc139@gmail.com>
2025-10-05 21:39:10 -0400

    [UPD] Update modal style

--- src/app/components/buttons/AddContact.tsx
+++ src/app/components/buttons/AddContact.tsx
@@ -55,14 +55,16 @@ export default function AddContact() {
             </div>
 
             {isOpen && (
-                <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-full max-h-full">
-                    <div className="relative p-4 w-full max-w-md max-h-full">
-                        {/* Modal content */}
-                        <div className="relative bg-white rounded-lg shadow-lg dark:bg-gray-700">
-                            {/* Modal header */}
-                            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
-                                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                                    Crear Nuevo Contacto
+                // <!-- Add User Modal -->
+                <div className="fixed inset-0 bg-black/60 top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-full max-h-full"
+                    id="add-user-modal">
+                    <div className="relative w-full max-w-2xl px-4 h-full md:h-auto">
+                        {/* <!-- Modal content --> */}
+                        <div className="bg-white rounded-lg shadow relative">
+                            {/* <!-- Modal header --> */}
+                            <div className="flex items-start justify-between p-5 border-b rounded-t">
+                                <h3 className="text-xl font-semibold">
+                                    Crear nuevo contacto
                                 </h3>
                                 <button
                                     type="button"
@@ -76,84 +78,88 @@ export default function AddContact() {
                             </div>
                             {/* Show error alert */}
                             {error && (
-                                <div className='bg-red-50 border border-red-200 text-red-700 px-4 py-3 rounded relative'>
-                                    <span className='block sm:inline'>{error}</span>
-                                    <button className='absolute top-0 bottom-0 right-0 px-4 py-3'
-                                        onClick={() => setError('')}>
-                                        <span className='text-2xl'>&times;</span>
-                                    </button>
+                                <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 mt-3 ml-3 mr-3" role="alert">
+                                    <p className="font-bold">Oops!!!</p>
+                                    <p>{error}</p>
                                 </div>
                             )}
-                            {/* Modal body */}
-                            <form onSubmit={onSubmit} className="p-4 md:p-5">
-                                <div className="grid gap-4 mb-4 grid-cols-2">
-
-                                    <div className="col-span-2">
-                                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
-                                        <input type="text"
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre completo"
-                                            disabled={isLoading}
-                                            {...register("name", {
-                                                required: {
-                                                    value: true,
-                                                    message: "El nombre es requerido"
-                                                }
-                                            })} />
-                                        {errors.name && (
-                                            <span className='text-red-500 text-xs'>
-                                                {String(errors.name.message)}
-                                            </span>
-                                        )}
+                            {/* <!-- Modal body --> */}
+                            <div className="p-6 space-y-6">
+                                <form onSubmit={onSubmit}>
+                                    <div className="grid grid-cols-6 gap-6">
+                                        <div className="col-span-6">
+                                            <label htmlFor="name"
+                                                className="text-sm font-medium text-gray-900 block mb-2">Nombre</label>
+                                            <input type="text"
+                                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                                                placeholder="Bonnie"
+                                                disabled={isLoading}
+                                                {...register("name", {
+                                                    required: {
+                                                        value: true,
+                                                        message: "El nombre es requerido"
+                                                    }
+                                                })} />
+                                            {errors.name && (
+                                                <span className='text-red-500 text-xs'>
+                                                    {String(errors.name.message)}
+                                                </span>
+                                            )}
+                                        </div>
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label htmlFor="email"
+                                                className="text-sm font-medium text-gray-900 block mb-2">Correo</label>
+                                            <input type="email"
+                                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                                                placeholder="example@mail.com"
+                                                {...register("email", {
+                                                    required: {
+                                                        value: true,
+                                                        message: "El correo es requerido"
+                                                    }
+                                                })} />
+                                            {errors.email && (
+                                                <span className='text-red-500 text-xs'>
+                                                    {String(errors.email.message)}
+                                                </span>
+                                            )}
+                                        </div>
+                                        <div className="col-span-6 sm:col-span-3">
+                                            <label htmlFor="phone"
+                                                className="text-sm font-medium text-gray-900 block mb-2">Teléfono</label>
+                                            <input type="text"
+                                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                                                placeholder="809-123-4567"
+                                                {...register("phone", {
+                                                    required: {
+                                                        value: true,
+                                                        message: "El teléfono es requerido"
+                                                    }
+                                                })} />
+                                            {errors.phone && (
+                                                <span className='text-red-500 text-xs'>
+                                                    {String(errors.phone.message)}
+                                                </span>
+                                            )}
+                                        </div>
                                     </div>
-
-                                    <div className="col-span-2">
-                                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                                        <input type="email"
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nombre@ejemplo.com"
-                                            {...register("email", {
-                                                required: {
-                                                    value: true,
-                                                    message: "El correo es requerido"
-                                                }
-                                            })} />
-                                        {errors.email && (
-                                            <span className='text-red-500 text-xs'>
-                                                {String(errors.email.message)}
-                                            </span>
-                                        )}
+                                    <div className='mt-7'>
+                                        <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            {isLoading ? (
+                                                <span className='flex items-center justify-center gap-2'>
+                                                    <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                                    </svg>
+                                                    Agregando contacto...
+                                                </span>
+                                            ) : (
+                                                'Agregar contacto'
+                                            )}
+                                        </button>
                                     </div>
-
-                                    <div className="col-span-2">
-                                        <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Teléfono</label>
-                                        <input type="tel"
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="809-123-4567"
-                                            {...register("phone", {
-                                                required: {
-                                                    value: true,
-                                                    message: "El correo es requerido"
-                                                }
-                                            })} />
-                                        {errors.phone && (
-                                            <span className='text-red-500 text-xs'>
-                                                {String(errors.phone.message)}
-                                            </span>
-                                        )}
-                                    </div>
-                                </div>
-                                <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    {isLoading ? (
-                                        <span className='flex items-center justify-center gap-2'>
-                                            <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                            </svg>
-                                            Agregando contacto...
-                                        </span>
-                                    ) : (
-                                        'Agregar contacto'
-                                    )}
-                                </button>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
