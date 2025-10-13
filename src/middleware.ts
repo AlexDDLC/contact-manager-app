adfb7cbab1fd67a2ceee81c4092d3cae62cf7a43
Robert Genao <trebor.1997@gmail.com>
2025-10-13 16:54:15 -0400

    [FIX] Excepcion para que encontrara imagenes en directorio

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,21 +3,18 @@ import { NextResponse } from "next/server";
 import type { NextRequestWithAuth } from "next-auth/middleware";
 
 export default withAuth(
-    function middleware(req: NextRequestWithAuth) {
-        return NextResponse.next();
-    },
-    {
-        pages: {
-            signIn: '/login',
-        },
-        callbacks: {
-            authorized: ({ token }) => !!token,
-        },
-    }
+  function middleware(req: NextRequestWithAuth) {
+    return NextResponse.next();
+  },
+  {
+    pages: { signIn: "/login" },
+    callbacks: { authorized: ({ token }) => !!token },
+  }
 );
 
 export const config = {
-    matcher: [
-        "/((?!api|_next/static|_next/image|favicon.ico|login|register).*)",
-    ],
+  matcher: [
+    "/((?!api|_next/static|_next/image|_next/data|favicon.ico|robots.txt|sitemap.xml|login|register|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico|css|js|txt|json|map|woff2?|ttf|eot|otf|mp4|webm|ogg|mp3|wav)$).*)",
+  ],
 };
+
